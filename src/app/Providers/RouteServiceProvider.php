14972a398bd4553b1149c820470c1cb8c57f4e4f
roiaki <roitoitoitoi@yahoo.co.jp>
2023-02-15 00:35:41 +0900

    chap41

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -18,6 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const OWNER_HOME = '/owner/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
 
     /**
      * The controller namespace for the application.
@@ -42,8 +44,24 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
+            
+            // routes/owner.php の全てのURLの頭にprefix('owner')が付く
+            Route::prefix('owner')
+                ->as('owner')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/owner.php'));
+            
+            // routes/admin.php の全てのURLの頭にprefix('admin')が付く
+            Route::prefix('admin')
+                ->as('admin')
+                ->middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
+            
+            Route::prefix('/')
+                ->as('user.')
+                ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
